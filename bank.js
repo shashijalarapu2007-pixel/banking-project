aafb042496bf3200ddb1161511da975693b3ce11
shashi <shashijalarapu2007@gmail.com>
2026-04-07 15:25:29 +0530

    Version 2

---
+++ bank.js
@@ -0,0 +1,7 @@
+function calculateSI(){
+    var p=document.getElementById('amt').value;
+    var n=document.getElementById('nper').value;
+    var r=document.getElementById('rate').value;
+    var si=(p*n*r)/100;
+    document.getElementById("res").innerHTML +=" " + si + " Rs.";
+} git a
